981dc018c14930ad72c5da79ca24b23f8444534c
nevroje <joakim.berglind@gmail.com>
2019-06-08 09:49:17 +0200

    added imports and connect to db, create readme

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask.ext.jsonpify import jsonify
+
+db_connect = create_engine('sqlite://chinook.db')
+app = Flask(__name__)
+api = Api(app)
