08029583fe521a58d17f13f12d6b997674b22a19
nevroje <joakim.berglind@gmail.com>
2019-06-08 10:02:51 +0200

    added routes for resources

--- server.py
+++ server.py
@@ -25,4 +25,10 @@ class Employees_name(Resource):
     def get(self, employee_id):
         conn = db_connect.connect()
         query = conn.execute("select * from employees where EmployeeId =%d " %int(employee_id))
-        
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+api.add_resource(Employees, '/employees') #route 1
+api.add_resource(Tracks, '/tracks') #route 2
+api.add_resource(Employees_Name, '/employees/<employee_id>') #route 3
+
